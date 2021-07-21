7009fb8f51281e51a69aab803a05503bfe69525e
bcollyer90 <bcollyer90@gmail.com>
2021-07-21 18:36:30 +0100

    updating

--- unsu.go
+++ unsu.go
@@ -8,31 +8,20 @@ import (
 	"strings"
 )
 
+func run(command string) {
+	cmd := exec.Command("bash", "-c", command)
+	stdout, _ := cmd.Output()
+	fmt.Println(string(stdout))
+	return
+}
+
 func main() {
-
 	r, _ := regexp.Compile("[sudo]{4}")
-
 	command := strings.Join(os.Args[1:], " ")
 
 	if r.MatchString(command) {
-		cmd := exec.Command("bash", "-c", strings.Replace(command, "sudo ", "", -1))
-		stdout, err := cmd.Output()
-
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		fmt.Println(string(stdout))
+		run(strings.Replace(command, "sudo ", "", -1))
 	} else {
-		cmd := exec.Command("bash", "-c", command)
-		stdout, err := cmd.Output()
-
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		fmt.Println(string(stdout))
+		run(command)
 	}
 }
